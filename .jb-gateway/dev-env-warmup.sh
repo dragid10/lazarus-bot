4f5ed5f9923996c9d9d1fd6cd7c74d9611769bd9
Alex Oladele <Dragid10@gmail.com>
2022-04-22 16:50:00 +0000

    Add jetbrains space related files for Remote developement

---
+++ .jb-gateway/dev-env-warmup.sh
@@ -0,0 +1,3 @@
+curl -sSL https://install.python-poetry.org | python3 - --preview
+source $HOME/.poetry/env
+export PATH='$PATH:/root/.local/bin'
---
+++ .space.kts
@@ -0,0 +1,11 @@
+// this job will run on every git push
+job("My project warmup data for Jetbrains Gateway") {
+    // ide is an IDE you want Space to build indexes for:
+    // for JetBrains Fleet - Ide.Fleet
+    // for IntelliJ-based IDEs via Gateway -
+    // Ide.Idea, Ide.WebStorm, Ide.RubyMine,
+    // Ide.CLion, Ide.GoLand, Ide.PhpStorm, Ide.PyCharm
+    warmup(ide = Ide.PyCharm) {
+        scriptLocation = ".jb-gateway/dev-env-warmup.sh"
+    }
+}
